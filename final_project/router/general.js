4cc7287f96761a98f7e12cfe8aa880dfd79dccf2
Ali Souri <souri7205@gmail.com>
2025-11-12 22:09:47 +0330

    implent get book based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,8 +26,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+  for (let i = 1; i < 11; i++){
+    if (books[i].author === author) {
+      res.send(JSON.stringify(books[i]))
+    }
+  }
+  return res.status(404).send(JSON.stringify({message: 'user doesnt exist in DB!'}))
 });
 
 // Get all books based on title
