2665f08311cf3dd02bdd69410d0658be97e16477
Ali Souri <souri7205@gmail.com>
2025-11-12 22:31:49 +0330

    implent get book reviews based on isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,8 +48,9 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn
+  res.send(JSON.stringify(books[isbn].reviews))
+  return res.status(200).json({ message: "successful" });
 });
 
 module.exports.general = public_users;
